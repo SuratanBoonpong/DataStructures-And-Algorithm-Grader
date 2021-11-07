0737e69a8836c82de548d197a82aba5226dc2935
Suratan63011017 <folnw2545@gmail.com>
2021-11-07 07:20:52 +0700

    Create 63011017_Lab09_2.py

---
+++ Lab9 Sorting/63011017_Lab09_2.py
@@ -0,0 +1,22 @@
+def findIndexOfMaxPositive(lists):
+    index = None
+    for i in range(len(lists)):
+        if index == None and lists[i] >= 0:
+            index = i
+        elif index != None and lists[i] >lists[index]:
+            index = i
+    return index
+
+def selectionSort(lists):
+    for i in range(len(lists)-1,-1,-1):
+        if lists[i] >= 0:
+            indexOflastPositive = i
+            swapIndex = findIndexOfMaxPositive(lists[:indexOflastPositive])
+            if swapIndex != None and lists[swapIndex]>=lists[indexOflastPositive]:
+                lists[indexOflastPositive],lists[swapIndex] = lists[swapIndex],lists[indexOflastPositive]
+    return lists
+
+lists = list(map(int,input("Enter Input : ").split(" ")))
+temp = selectionSort(lists)
+for i in temp:
+    print(i,end=" ")
